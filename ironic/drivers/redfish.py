da6ab66d711ff6c28d0b7204782fe0fdb1607961
Seunghun Lee <seunghun@stackhpc.com>
2025-01-22 13:35:06 +0000

    redfish appliance merge to yoga (#2)
    * Add Network-only deploy driver
    
    Deploy driver that only calls the network driver.
    If configured, reboot via redfish after each network change.
    
    * Tidy ups for pep8
    
    * Fix redfish-network-appliance hardware type
    
    * Add missing interfaces
    
    * Add missing return value
    
    * Remove duplicated imports
    
    ---------
    
    Co-authored-by: John Garbutt <john.garbutt@stackhpc.com>
    (cherry picked from commit 7fe8146)

--- ironic/drivers/redfish.py
+++ ironic/drivers/redfish.py
@@ -13,9 +13,16 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from ironic.common import states
+from ironic.drivers import base
 from ironic.drivers import generic
+from ironic.drivers import hardware_type
 from ironic.drivers.modules import agent
+from ironic.drivers.modules import fake
 from ironic.drivers.modules import ipxe
+from ironic.drivers.modules.network import flat as flat_net
+from ironic.drivers.modules.network import neutron
+from ironic.drivers.modules.network import noop as noop_net
 from ironic.drivers.modules import noop
 from ironic.drivers.modules import noop_mgmt
 from ironic.drivers.modules import pxe
@@ -27,6 +34,7 @@ from ironic.drivers.modules.redfish import management as redfish_mgmt
 from ironic.drivers.modules.redfish import power as redfish_power
 from ironic.drivers.modules.redfish import raid as redfish_raid
 from ironic.drivers.modules.redfish import vendor as redfish_vendor
+from ironic.drivers.modules.storage import noop as noop_storage
 
 
 class RedfishHardware(generic.GenericHardware):
@@ -75,3 +83,69 @@ class RedfishHardware(generic.GenericHardware):
     @property
     def supported_firmware_interfaces(self):
         return [redfish_firmware.RedfishFirmware, noop.NoFirmware]
+
+
+class RedfishNetworkAppliance(hardware_type.AbstractHardwareType):
+    """Redfish appliance moved between networks and rebooted using Ironic."""
+
+    @property
+    def supported_power_interfaces(self):
+        return [redfish_power.RedfishPower, fake.FakePower]
+
+    @property
+    def supported_inspect_interfaces(self):
+        """List of supported power interfaces."""
+        # TODO(johng): maybe we only want the port detection?
+        return [redfish_inspect.RedfishInspect, noop.NoInspect]
+
+    @property
+    def supported_network_interfaces(self):
+        """List of supported network interfaces."""
+        return [neutron.NeutronNetwork, flat_net.FlatNetwork,
+                noop_net.NoopNetwork]
+
+    @property
+    def supported_boot_interfaces(self):
+        """List of classes of supported boot interfaces."""
+        return [fake.FakeBoot]
+
+    @property
+    def supported_deploy_interfaces(self):
+        """List of supported deploy interfaces."""
+        return [NetworkOnlyDeploy]
+
+    @property
+    def supported_management_interfaces(self):
+        return [noop_mgmt.NoopManagement]
+
+    @property
+    def supported_raid_interfaces(self):
+        return [noop.NoRAID]
+
+    @property
+    def supported_rescue_interfaces(self):
+        return [noop.NoRescue]
+
+    @property
+    def supported_storage_interfaces(self):
+        return [noop_storage.NoopStorage]
+
+
+class NetworkOnlyDeploy(fake.FakeDeploy):
+    """Class for only doing the network part of a typical deployment.
+
+    This does only the network setup,
+    then (optionally?) reboots the appliance via redfish,
+    letting DHCP do the heavy lifting.
+    """
+
+    @base.deploy_step(priority=100)
+    def deploy(self, task):
+        task.driver.network.configure_tenant_networks(task)
+        task.driver.power.reboot(task)
+
+    def tear_down(self, task):
+        task.driver.network.unconfigure_tenant_networks(task)
+        # TODO(johng): should we power it off?
+        task.driver.power.reboot(task)
+        return states.DELETED
