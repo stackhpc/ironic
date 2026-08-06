c63f3ae09da21fa68dad80b59f90a8cf084a781e
Doug Szumski <doug@stackhpc.com>
2026-08-06 14:01:34 +0100

    Apply suggestion from @dougszumski

--- ironic/conductor/cleaning.py
+++ ironic/conductor/cleaning.py
@@ -285,7 +285,6 @@ def do_next_clean_step(task, step_index, disable_ramdisk=None):
 
     # Switch back to original interface, if necessary
     task.driver.deploy.restore_interface(task)
-    
     event = 'manage' if manual_clean or node.retired else 'done'
     # NOTE(rloo): No need to specify target prov. state; we're done
     task.process_event(event)
